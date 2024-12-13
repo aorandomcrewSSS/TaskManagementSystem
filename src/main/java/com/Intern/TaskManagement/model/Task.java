ab3a6b70e013ac60480fbf4d83f54694d9ebc391
Ao- <153730221+aorandomcrewSSS@users.noreply.github.com>
2024-12-14 02:45:52 +0300

    Update Task.java

--- src/main/java/com/Intern/TaskManagement/model/Task.java
+++ src/main/java/com/Intern/TaskManagement/model/Task.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Setter
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
