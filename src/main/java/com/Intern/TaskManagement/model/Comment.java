a61438c725c35a9d1c9cc4b86075c9542c105432
Ao- <153730221+aorandomcrewSSS@users.noreply.github.com>
2024-12-14 02:46:04 +0300

    Update Comment.java

--- src/main/java/com/Intern/TaskManagement/model/Comment.java
+++ src/main/java/com/Intern/TaskManagement/model/Comment.java
@@ -13,7 +13,7 @@ import lombok.*;
 @Setter
 public class Comment {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
